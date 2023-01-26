78ce4e60b698879bc3c26486ba2e85e15fa7432a
SameedHasan <64144024+SameedHasan@users.noreply.github.com>
2023-01-26 15:25:15 +0500

    basename removed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Home from "./components/Home";
 const App = () => {
   return (
     <div>
-      <BrowserRouter basename="/">
+      <BrowserRouter>
         <div>
           <nav>
             <ul>
