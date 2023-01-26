56da50d3f2f62468560a7b2e0b5ae8be71a6bad2
SameedHasan <64144024+SameedHasan@users.noreply.github.com>
2023-01-26 15:27:03 +0500

    exact

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ const App = () => {
             </ul>
           </nav>
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route exact path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/profile" element={<Profile />} />
